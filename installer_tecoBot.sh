c28f9ef3fa5e1e87bd2355bbc5325855f0c47610
Niklas Vorberg | Paints <n.vorberg@paints.de>
2018-03-15 15:55:27 +0100

    [TASK] small update of the installer

--- installer_tecoBot.sh
+++ installer_tecoBot.sh
@@ -9,7 +9,26 @@ if [ "$userinput" = "yes" ]
 then
     echo "Starting the ínstaller"
 
-    mkdir discord && cd discord && git clone https://github.com/ZombyMedia/tecobot.git && cd tecobot && npm install && echo "Installation completed"
+    file="/etc/hosts"
+    if [ -f "$file" ]
+    then
+    	echo "$file allready exists."
+
+        echo "Do you want to try an update of the package"
+        echo "Type yes or no"
+
+        read userinput
+        if [ "$userinput" = "yes" ]
+        then
+            git fetch && git pull && cd discord/tecobot && npm install
+        else
+            echo "Installer script is now stopped"
+        fi
+    else
+    	echo "$file does not exists. Installing package from github..."
+        mkdir discord && cd discord && git clone https://github.com/ZombyMedia/tecobot.git && cd tecobot && npm install && echo "Installation completed"
+    fi
+
 else
     echo "Installer script is now stopped"
 fi
